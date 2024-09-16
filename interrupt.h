bf978b3d2c0dfd12150add66462957c14283a4eb
Kota-James <n.kota.all.01@gmail.com>
2024-09-16 17:57:36 +0900

    NucleoMouse2024標準プログラム-ヘッダーファイル

---
+++ interrupt.h
@@ -0,0 +1,15 @@
+/*
+ * interrupt.h
+ *
+ *  Created on: Aug 31, 2024
+ *      Author: nkota
+ */
+
+#ifndef INC_INTERRUPT_H_
+#define INC_INTERRUPT_H_
+
+
+void tim6_wait_us(uint32_t);
+
+
+#endif /* INC_INTERRUPT_H_ */
