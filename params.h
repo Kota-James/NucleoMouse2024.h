0c3250cefe727d33b87992c0fced93f4f474ada9
Kota-James <n.kota.all.01@gmail.com>
2024-09-22 16:16:54 +0900

    t

--- params.h
+++ params.h
@@ -16,11 +16,11 @@
 ------------------------------------------------------------*/
 //----走行パルス関連----
 #define PULSE_SEC_HALF 218 // 半区画走行用パルス。2倍すると1区画分に
-#define PULSE_ROT_R90 155     // 右90度回転用パルス数
+#define PULSE_ROT_R90 154     // 右90度回転用パルス数
 #define PULSE_ROT_L90 155     // 左90度回転用パルス数
 #define PULSE_ROT_IN 146      //スラローム走行時の内側走行パルス数
 #define PULSE_ROT_OUT 448     //スラローム走行時の外側走行パルス数
-#define PULSE_OFFSET 40       //スラローム前後のオフセット区間
+#define PULSE_OFFSET 39       //スラローム前後のオフセット区間
 #define PULSE_ROT_180 310     // 180度回転用パルス数
 #define PULSE_SETPOS_BACK 200 // 後ろ壁に当てるために下がるパルス数
 #define PULSE_SETPOS_SET 94 // 後ろ壁から中央までのパルス数
@@ -59,11 +59,12 @@
 // 33kΩと10kΩの分圧抵抗を通してバッテリ電圧を取得している→ 11.1*(10/(10+33)/3.3)*4096=3204
 
 //----制御閾値（しきい値）----
-#define RAPID_CHANGE 200  //急激な変化量の基準値
+#define RAPID_CHANGE 250  //急激な変化量の基準値
 #define CTRL_BASE_L 100 // 左制御閾値
 #define CTRL_BASE_R 100 // 右制御閾値
-#define CTRL_MAX 250    // 制御量上限値
-#define CTRL_CONT 0.20F  // 比例制御係数
+#define CTRL_MAX 200    // 制御量上限値
+#define CTRL_P 0.25  // 比例制御係数
+#define CTRL_D  0.00  //微分制御係数
 
 //----赤外線（赤色）LED発光待機時間（単位はマイクロ秒）
 #define IR_WAIT_US 15
