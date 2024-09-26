513d2949185bf0e581f86b10efbbf5ab5b4a2f7e
Kota-James <n.kota.all.01@gmail.com>
2024-09-26 16:33:41 +0900

    モードによる制御量変更をいったん元に戻した

--- interrupt.h
+++ interrupt.h
@@ -8,28 +8,7 @@
 #ifndef INC_INTERRUPT_H_
 #define INC_INTERRUPT_H_
 
-
-//====変数====
-#ifdef MAIN_C_              //main.cからこのファイルが呼ばれている場合
-  /*グローバル変数の定義*/
-  //volatile int16_t dl_tmp, dr_tmp;
-  //volatile uint16_t wall_base_l, wall_base_r;
-  //volatile uint16_t ctrl_base_l, ctrl_base_r;
-  //volatile uint16_t ctrl_max, ctrl_p, ctrl_d,
-  volatile int16_t ctrl_amount_kid, ctrl_amount_mother;
-#else                       //main.c意外からこのファイルが呼ばれている場合
-  /*グローバル変数の定義*/
-  //extern volatile int16_t dl_tmp, dr_tmp;
-  //extern volatile uint16_t wall_base_l, wall_base_r;
-  //extern volatile uint16_t ctrl_base_l, ctrl_base_r;
-  //extern volatile uint16_t ctrl_max, ctrl_p, ctrl_d,
-  extern volatile int16_t ctrl_amount_kid, ctrl_amount_mother;
-#endif
-
-
 void tim6_wait_us(uint32_t);
-
-void interrupt_init(void);    //制御系変数初期化関数
 
 
 #endif /* INC_INTERRUPT_H_ */
